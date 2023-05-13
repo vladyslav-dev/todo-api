e3d0e0afbc74496e01f43281cf1ea4b3e32dd582
Vladyslav Hryhorenko <grigovlad09112002@gmail.com>
2023-05-13 21:40:32 +0300

    New app package with setup.go

---
+++ app/setup.go
@@ -0,0 +1,21 @@
+package app
+
+func SetupAndRunApp() error {
+	// Load env
+
+	// Start database
+
+	// Defer closing database
+
+	// Create App
+
+	// Middlewares
+
+	// Routes
+
+	// Swagger
+
+	// Run App
+
+	return nil
+}
